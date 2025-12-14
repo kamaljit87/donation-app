2b21f5becc54997ba125e3521026d9666b5c220d
Ubuntu <ubuntu@ip-172-31-33-146.ec2.internal>
2025-12-14 19:26:39 +0000

    Fix admin panel - restore missing Laravel core files and configurations

--- backend/config/services.php
+++ backend/config/services.php
@@ -6,6 +6,12 @@ return [
     |--------------------------------------------------------------------------
     | Third Party Services
     |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
     */
 
     'mailgun' => [
@@ -23,11 +29,6 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
-    'razorpay' => [
-        'key' => env('RAZORPAY_KEY_ID'),
-        'secret' => env('RAZORPAY_KEY_SECRET'),
     ],
 
 ];
